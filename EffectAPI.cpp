ca5bcf59444be0ceeb6ac90d0a21fcbd0cbf8a38
Chuck Walbourn <chuckw@windows.microsoft.com>
2018-02-23 23:19:59 -0800

    Fixed up comment header block

--- EffectAPI.cpp
+++ EffectAPI.cpp
@@ -3,12 +3,8 @@
 //
 // Effect API entry point
 //
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 //
 // http://go.microsoft.com/fwlink/p/?LinkId=271568
 //--------------------------------------------------------------------------------------
--- EffectNonRuntime.cpp
+++ EffectNonRuntime.cpp
@@ -6,12 +6,8 @@
 // are typically called when creating, cloning, or optimizing an 
 // Effect, or reflecting a variable.
 //
-// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
-// PARTICULAR PURPOSE.
-//
 // Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 //
 // http://go.microsoft.com/fwlink/p/?LinkId=271568
 //--------------------------------------------------------------------------------------
