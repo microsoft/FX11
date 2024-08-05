98fb4d48e76d5549abb23c7ee0179ffa7cc559ef
Chuck Walbourn <walbourn@users.noreply.github.com>
2024-08-05 12:38:25 -0700

    Fixed CodeQL warnings (#27)

--- EffectLoad.cpp
+++ EffectLoad.cpp
@@ -1916,6 +1916,14 @@ HRESULT CEffectLoader::LoadAssignments( uint32_t Assignments, SAssignment **ppAs
             case ELHS_GeometryShaderSO:
                 assert(0); // Should never happen
 
+            #if (__cplusplus >= 201703L)
+                [[fallthrough]];
+            #elif defined(__clang__)
+                [[clang::fallthrough]];
+            #elif defined(_MSC_VER)
+                __fallthrough;
+            #endif
+
             default:
                 VHD( E_FAIL, "Internal loading error: invalid shader type."  );
             }
